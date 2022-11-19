aa6600b5bf2d074ebf7a260707993e9802deafaa
Ekatmoz <ekatmoz@gmail.com>
2022-11-19 18:27:24 +0100

    first cmmit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+
+import { useState } from 'react';
+import './App.css';
+import { data } from './data'
+
+function App() {
+
+  const [person, setPerson] = useState(0); //- because we are not showing the whole array we will show just i person at a time, 0 - because counting starts from 0
+  const {id, name, description, age, image} = data[person];
+
+  const previousPerson = () => {
+      setPerson((person => {
+        person--;
+        if (person < 0){
+          return data.length - 1; //this means last 
+        }
+        return person;
+      }))
+  }
+
+  const nextPerson = () => {
+    setPerson((person =>{
+      person++;
+      if (person > data.length - 1) {
+        person = 0;
+      }
+      return person;
+      
+    }))
+
+  }
+
+  return (
+    <div>
+      <div className='container'>
+        <img src={image} width='300px' alt='man'/>
+      </div>
+
+      <div className='container'>
+        <h1>{id} - {name}</h1>
+      </div>
+
+      <div className='container'>
+        <h2>{description}</h2>
+      </div>
+
+      <div className='container'>
+        <h2>{age} years old</h2>
+      </div>
+
+      <div className='btn container'>
+        <button onClick={previousPerson}>Previous</button>
+        <button onClick={nextPerson}>Next</button>
+      </div>
+
+    </div>
+  );
+}
+
+export default App;
